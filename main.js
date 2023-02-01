c55589c5d3cc44c56cf8734df96f6910d1296a4c
Agustin Mayol <agusmayolitos@gmail.com>
2023-02-01 02:13:35 -0300

    Optimizando código

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
-let anc = document.getElementById("anc");
-let disabled = document.getElementById("disabled");
-let transparence = document.getElementById("transparence");
+const anc = document.getElementById("anc");
+const disabled = document.getElementById("disabled");
+const transparence = document.getElementById("transparence");
 
 let stream;
+let audioContext;
+let noiseSuppressor;
 
 let ancEnabled = true;
 
-anc.addEventListener("click", function () {
+async function startNoiseCancellation() {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const source = audioContext.createMediaStreamSource(stream);
+    noiseSuppressor = audioContext.createScriptProcessor(4096, 1, 1);
+
+    noiseSuppressor.onaudioprocess = function (event) {
+        const inputData = event.inputBuffer.getChannelData(0);
+        const outputData = event.outputBuffer.getChannelData(0);
+
+        // Aplicar la función de cancelación de ruido aquí
+
+        for (let i = 0; i < inputData.length; i++) {
+            outputData[i] = inputData[i];
+        }
+    };
+
+    source.connect(noiseSuppressor);
+    noiseSuppressor.connect(audioContext.destination);
+}
+
+anc.addEventListener("click", async function () {
     if (ancEnabled) {
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(newStream => {
-                stream = newStream;
-                const audioContext = new AudioContext();
-                const source = audioContext.createMediaStreamSource(stream);
-                const noiseSuppressor = audioContext.createScriptProcessor(4096, 1, 1);
-
-                noiseSuppressor.onaudioprocess = function (event) {
-                    const inputData = event.inputBuffer.getChannelData(0);
-                    const outputData = event.outputBuffer.getChannelData(0);
-
-                    // Aplicar la función de cancelación de ruido aquí
-
-                    for (let i = 0; i < inputData.length; i++) {
-                        outputData[i] = inputData[i];
-                    }
-                };
-
-                source.connect(noiseSuppressor);
-                noiseSuppressor.connect(audioContext.destination);
-            })
-            .catch(console.error);
+        await startNoiseCancellation();
     }
 });
 
@@ -38,6 +40,7 @@ disabled.addEventListener("click", function () {
     if (!ancEnabled && stream) {
         stream.getTracks().forEach(track => track.stop());
         stream = null;
+        noiseSuppressor.disconnect();
     }
 });
 
